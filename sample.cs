51a71cf0e57ddd7a042359be16d213c91b29be8d
kamaleshvnco <kamalesh.vasanthnco@gmail.com>
2026-05-03 18:39:42 +0530

    feat: added a product

--- sample.cs
+++ sample.cs
@@ -2,3 +2,8 @@ int a = 10;
 int b = 20;
 int sum = a + b;
 Console.WriteLine("The sum of a and b is: " + sum);
+
+int a = 40;
+int b = 30;
+int product = a * b;
+Console.WriteLine("The product of a and b is: " + product);
